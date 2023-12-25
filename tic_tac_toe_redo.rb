4467c8e811db1756de9624700917b844a61f27cd
Stefano <snebo54@hotmail.com>
2023-12-25 17:57:02 +0100

    Added win loose functionality

--- tic_tac_toe_redo.rb
+++ tic_tac_toe_redo.rb
@@ -14,9 +14,9 @@ class Game
       if i.even?
         3.times do |j|
           # Replace with p symbols
-          if p1.spots.include?(num.to_s)
+          if p1.spots.include?(num)
             print "#{space}#{p1.sym}#{space}"
-          elsif p2.spots.include?(num.to_s)
+          elsif p2.spots.include?(num)
             print "#{space}#{p2.sym}#{space}"
           else
             print "#{space}#{num}#{space}"
@@ -32,17 +32,23 @@ class Game
 
   def play(p1, p2)
     turns = 0
-    self.draw_board(p1,p2)
+    self.draw_board(p1, p2)
     while not @game_end
       if turns < 9
         @turn ? self.ch_turn(p1) : self.ch_turn(p2)
-        self.draw_board(p1,p2)
+        !@turn ? self.has_won(p1) : self.has_won(p2)
+        #stop from drawing the board on game over
+        @game_end ? nil : self.draw_board(p1, p2)
+
+        @turn = not(@turn)
         turns += 1
+
       else
         @game_end = true
+        puts 'There are no avaliable turns left... game over'
       end
     end
-    puts 'There are no avaliable turns left... game over'
+    puts "Do you want to play again? (y,n)"
   end
 
   def ch_turn(p)
@@ -52,9 +58,25 @@ class Game
       print "#{p.name.capitalize} enter a valid no -> "
       choice = gets.chomp
     end
-    p.store_turn(choice)
+    p.store_turn(choice.to_i)
     @avl_p.append(choice.to_i)
-    @turn = not(@turn)
+    # self.has_won(p)
+  end
+
+  def has_won(pl)
+    win = false
+    win_spots = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 8],
+                 [1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
+    win_spots.each do |i|
+      i.all? {|j| pl.spots.include?(j)} ? win = true : nil
+      # pl.spots.sort.eql?(i) ? win = true : nil
+    end
+    if win
+      puts "#{pl.name.capitalize} Won!..."
+      pl.add_score
+      @game_end = true
+    end
   end
 end
 
@@ -73,6 +95,10 @@ class Player
 
   def store_turn(spot)
     @pl_cells.push(spot)
+  end
+
+  def add_score
+    @pl_score += 1
   end
   
   def spots
